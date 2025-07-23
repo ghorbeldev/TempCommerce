45c67c81c338f8cba1a365fb08cc4e34b75e233e
ghorbeldev <adem.ghorbel9@gmail.com>
2025-07-23 20:21:21 +0100

    add Order functionality

---
+++ app/api/order/create/route.js
@@ -0,0 +1,53 @@
+import connectDB from '@/config/db';
+import { inngest } from '@/config/inngest';
+import Order from '@/models/Order';
+import Product from '@/models/Product';
+import User from '@/models/User';
+import { getAuth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+import { useTransition } from 'react';
+
+export async function POST(request) {
+	try {
+		const { userId } = getAuth(request);
+		const { address, items } = await request.json();
+
+		if (!address || items.length === 0) {
+			return NextResponse.json({
+				success: false,
+				message: 'Invalid data',
+			});
+		}
+
+		await connectDB();
+		const amount = items.reduce(async (acc, item) => {
+			const product = await Product.findById(item.product);
+
+			return acc + product.offerPrice * item.quantity;
+		}, 0);
+
+		await inngest.send({
+			name: 'order/created',
+			data: {
+				userId,
+				address,
+				items,
+				amount: amount + Math.floor(amount * 0.02),
+				date: Date.now(),
+			},
+		});
+		const user = await User.findById(userId);
+		user.cartItems = {};
+		await user.save();
+		return NextResponse.json({
+			success: true,
+			message: 'Order placed successfully',
+		});
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json({
+			success: false,
+			message: error.message,
+		});
+	}
+}
---
+++ models/Order.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema({
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	items: [
+		{
+			product: {
+				type: String,
+				required: true,
+				ref: 'product',
+			},
+			quantity: {
+				type: Number,
+				required: true,
+			},
+		},
+	],
+	amount: { type: Number, required: true },
+	address: { type: String, required: true, ref: 'address' },
+	status: { type: String, required: true, default: 'Order Placed' },
+	date: { type: Number, required: true },
+});
+
+const Order = mongoose.models.order || mongoose.model('order', orderSchema);
+
+export default Order;
